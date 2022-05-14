c294dd139e5681acaf0fcfd5984405d923ad2329
boukharoubamehdi <boukharouba.mehdi@gmail.com>
2022-05-14 16:43:37 -0400

    Refactoring & Implementing CurrencyExchangeRepository (#3)

--- src/main/java/com/mb/rest/webservices/currencyexchangeservice/currencyexchange/Controller.java
+++ src/main/java/com/mb/rest/webservices/currencyexchangeservice/currencyexchange/Controller.java
@@ -1,12 +1,10 @@
-package com.mb.rest.webservices.currencyexchangeservice.controller;
+package com.mb.rest.webservices.currencyexchangeservice.currencyexchange;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.math.BigDecimal;
 
 // We'll create all the components in the same package.
 // Ideally, if you have a large project, you would want to create separate packages for controllers,
@@ -15,13 +13,21 @@ import java.math.BigDecimal;
 @RestController
 public class Controller {
 
+  @Autowired CurrencyExchangeRepository currencyExchangeRepository;
   @Autowired private Environment environment;
 
   @GetMapping("/currency-exchange/from/{from}/to/{to}")
   public CurrencyExchange retrieveExchangeValue(
       @PathVariable String from, @PathVariable String to) {
 
+    CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
+
+    if (currencyExchange == null) {
+      throw new RuntimeException("Unable to find data for " + from + "to " + to);
+    }
     String port = environment.getProperty("local.server.port");
-    return new CurrencyExchange(2000L, from, to, BigDecimal.valueOf(1.29), port);
+    currencyExchange.setEnvironment(port);
+
+    return currencyExchange;
   }
 }
